377591712dcb78f075bba57da09f71ee1b9edb1b
Raymond Bian <raybbian@gmail.com>
2026-04-06 19:40:38 -0400

    oops

--- Hypermania/Assets/Scripts/Scenes/Menus/InputSelect/InputSelectDirectory.cs
+++ Hypermania/Assets/Scripts/Scenes/Menus/InputSelect/InputSelectDirectory.cs
@@ -92,7 +92,7 @@ namespace Scenes.Menus.InputSelect
                 options.LocalPlayers[i] = new LocalPlayerOptions
                 {
                     InputDevice = i == 0 ? p1 : p2,
-                    Controls = _controlsConfigs?.Length <= i + 1 ? _controlsConfigs[i] : null,
+                    Controls = _controlsConfigs?.Length >= i + 1 ? _controlsConfigs[i] : null,
                 };
             }
             options.InfoOptions = new InfoOptions { ShowFrameData = SessionDirectory.Config == GameConfig.Training };
